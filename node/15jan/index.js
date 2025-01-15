b42dea2acb7781fd245e1e4301201a2e4b678f5d
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-15 15:18:29 +0530

    basic

--- node/15jan/index.js
+++ node/15jan/index.js
@@ -4,10 +4,13 @@ const app=express();
 app.get('/',(req,res)=>{
 res.send("<h1> This is home page </h1>")
 })
-app.get('/about',(req,res)=>{
-    
+app.get('/about',(req,res)=>{  
     res.send("<h1> This is about page </h1>")
     })
+
+    app.get('/contact',(req,res)=>{  
+        res.send("<h1> This is contact page </h1>")
+        })
 
 app.listen(3000,()=>{
     console.log("server is running on port 3000")
