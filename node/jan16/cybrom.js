a1a657e0c7faf4cbb2d9d295748094ca0d63ebe2
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-16 15:36:07 +0530

    module

---
+++ node/jan16/cybrom.js
@@ -0,0 +1,12 @@
+ const Myclg=(()=>{
+    return "<h1> My college is NRI </h1>"
+ })
+
+ const MyCourse=(()=>{
+    return "<h1> My course is BTech </h1>"
+ })
+
+ module.exports = {
+    Myclg,
+    MyCourse
+ }
--- node/jan16/index.js
+++ node/jan16/index.js
@@ -1,7 +1,14 @@
 const http=require('http');
+const college=require('./cybrom')
 http.createServer((req,res)=>{
 res.write("<h1>This is my first node Program </h1>")
-res.end("<h3>Server End </h3>");
+
+res.write(college.Myclg())
+res.write(college.MyCourse())
+
+
+res.end("<p>Server End Here </p>");
+
 }).listen(4000,()=>{
     console.log("Server is running on port 4000")
-})
+}) 
