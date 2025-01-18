c4e40b5418d50425ad8f451b49c7f0672ebb951e
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-18 14:53:00 +0530

    npm

--- node/jan18/index.js
+++ node/jan18/index.js
@@ -2,5 +2,7 @@ const http=require('http');
 const uc=require('uppercase')
 http.createServer((req,res)=>{
     res.write(uc("<h1> Hello Guys </h1>"))
-    res.end();
-}).listen(3000);
+    res.end(uc("<p>Server end....... </p>"));
+}).listen(3000,()=>{
+    console.log("Sever run at 3000 port")
+});
