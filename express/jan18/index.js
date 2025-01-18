3bea8e14d5be4097586643798b7168d5e4614ea9
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-18 15:07:50 +0530

    express

---
+++ express/jan18/index.js
@@ -0,0 +1,8 @@
+const express=require('express')
+const app=express() 
+app.get('/',(req,res)=>{
+    res.send('<h1> hello world </h1>')
+})
+app.listen(8000,()=>{
+    console.log('server is running on port 8000')
+})
