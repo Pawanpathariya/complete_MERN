28fdfb57367983660797c52d9c641188cc397a8a
Pawanpathariya <pawanpathariys@gmail.com>
2025-01-27 16:06:33 +0530

    search and display

--- express/jan18/index.js
+++ express/jan18/index.js
@@ -1,8 +1,63 @@
+// const express=require('express')
+// const app=express() 
+
+
+// app.get('/',(req,res)=>{
+//     res.send('<h1> Hello world </h1>')
+// })
+// app.get('/home',(req,res)=>{
+//     res.send("<h1>Home page </h1>")
+// })
+// app.post('/savedata',(req,res)=>{
+//     res.send("this is save data post request ")
+// })
+// app.get('/about',(req,res)=>{
+//     res.send("<h1>About page </h1>")
+// })
+
+// app.get('/contact',(req,res)=>{
+//     res.send("<h1>Contact page </h1>")
+// })
+
+// app.post('/help',(req,res)=>{
+//     res.send("<h1>Help page </h1>")
+// })
+
+// app.post('/feedback',(req,res)=>{
+//     res.send("<h1>Feedback page </h1>")
+// })
+// app.listen(8000,()=>{
+//     console.log('server is running on port 8000')
+// }) 
+
+
+//routes
 const express=require('express')
-const app=express() 
-app.get('/',(req,res)=>{
-    res.send('<h1> hello world </h1>')
+ const app=express() 
+const port=8000;
+
+
+app.get('/student/home',(req,res)=>{
+    res.send("<h1>Student Home page </h1>")
 })
-app.listen(8000,()=>{
-    console.log('server is running on port 8000')
+app.get('/student/about',(req,res)=>{
+    res.send("<h1>Student About page </h1>")
 })
+app.get('/student/contact',(req,res)=>{
+    res.send("<h1>Student Contact page </h1>")
+})
+
+
+app.get('/teacher/home',(req,res)=>{
+    res.send("<h1>Teacher Home page </h1>")
+})
+app.get('/teacher/about',(req,res)=>{
+    res.send("<h1>Teacher About page </h1>")
+})
+app.get('/teacher/contact',(req,res)=>{
+    res.send("<h1>Teacher Contact page </h1>")
+})
+
+ app.listen(port,()=>{
+     console.log(`server is running on port ${port}`)
+ })
